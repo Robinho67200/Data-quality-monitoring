2e9929a9030d0adaa8a4a07388816ddd1412f5e7
Robin <jochem-robin@hotmail.fr>
2025-03-11 18:39:09 +0100

    Modification of the tests_functions.py file: 1- Updated values 2- Added a test to test dictionary generation.

--- src/tests/tests_functions.py
+++ src/tests/tests_functions.py
@@ -1,38 +1,36 @@
 import unittest
-
-import numpy as np
 from pandas import DataFrame
-from src.sensor import Visitors
 from datetime import datetime
-
+from src.sensor import Visitors
 
 class TestVisitSensor(unittest.TestCase):
     def test_generate_dataframe(self):
         visitors = Visitors()
-        self.assertEqual(type(visitors.generate_data()), DataFrame)
+        self.assertEqual(type(visitors.generate_data("df")), DataFrame)
+
+    def test_generate_dictionary(self):
+        visitors = Visitors()
+        self.assertEqual(type(visitors.generate_data("dict")), dict)
 
     def test_number_of_day(self):
         visitors = Visitors()
         nb_day = (datetime.now() - datetime(2020, 1, 1)).days
-        df = visitors.generate_data()
+        df = visitors.generate_data("df")
         nb_day_dataframe = df.groupby("day").count().shape[0]
 
         self.assertEqual(nb_day, nb_day_dataframe)
 
     def test_sunday_closed(self):
         visitors = Visitors()
-        visit_count = visitors.get_number_visitors("2025-03-02", 10)
-        self.assertTrue(visit_count)
+        visit_count = visitors.get_number_visitors("2025-03-02", 10, "Strasbourg")
+        self.assertFalse(visit_count)
 
     def test_day_open(self):
         visitors = Visitors()
-        visit_count = visitors.get_number_visitors("2025-03-03", 10)
-        self.assertEqual(visit_count, 72)
+        visit_count = visitors.get_number_visitors("2025-03-03", 10, "Strasbourg")
+        self.assertEqual(visit_count, 47)
 
     def test_breakdown(self):
         visitors = Visitors()
-        visit_count = visitors.get_number_visitors("2020-01-13", 12)
-        print(f"visit count : {visit_count}")
+        visit_count = visitors.get_number_visitors("2020-01-13", 12, "Strasbourg")
         self.assertTrue(visit_count)
-
-# git commit -m "Add 4 tests with unitest (test_generate_dataframe, test_number_of_day, test_sunday_closed, test_day_open"
