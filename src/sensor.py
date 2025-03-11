b28bbaa21cc9d310f5ebc38940fd88f46bd64e4e
Robin <jochem-robin@hotmail.fr>
2025-03-11 14:22:18 +0100

    Modification of the sensor.py file: 1- Modification of the add_data function to add more visitors on Saturday, at noon and in the evening. 2- Modification of the generate_data method to add the choice on the type of data returned (dictionary or dataframe). 3- Modification of the get_all_data_day method to process the data in dictionary form to be faster.

--- src/sensor.py
+++ src/sensor.py
@@ -28,19 +28,39 @@ def add_data(
     data["day"].append(new_day.strftime("%Y-%m-%d"))
     data["hour"].append(hour)
 
+    random_coefficient = random.uniform(0.7, 1.0)
+    saturday_coefficient = random.uniform(1.5, 2.0)
+    lunchtime_or_evening_coefficient = random.uniform(1.2, 1.4)
+    day_of_week = new_day.strftime("%w")
+
     if random.random() <= 0.05:  # 5% error
         data["id_sensor"].append("null")
     else:
         data["id_sensor"].append(sensor)
 
-    if int(new_day.strftime("%w")) == 0:  # Store close the Sunday
+    if int(day_of_week) == 6 and (
+        hour == 12 or hour >= 18
+    ):  # More customers on Saturday
+        nb_visitors = random.randint(20, 100) * saturday_coefficient
+    elif (
+        hour == 12 or hour >= 18
+    ):  # More customers at lunchtime and evening for the rest of the week
+        nb_visitors = (
+            random.randint(20, 100)
+            * saturday_coefficient
+            * lunchtime_or_evening_coefficient
+        )
+    else:
+        nb_visitors = random.randint(20, 100) * random_coefficient
+
+    if int(day_of_week) == 0:  # Store close the Sunday
         nb_visitors = "null"
     elif random.random() <= 0.02:  # 2% failure
         nb_visitors = "null"
     elif random.random() >= 0.95:  # 5% of improbable data
-        nb_visitors = random.randint(20, 100) * coef_improbable_data
-    else:
-        nb_visitors = random.randint(20, 100)
+        nb_visitors = (
+            random.randint(20, 100) * coef_improbable_data * random_coefficient
+        )
 
     if nb_visitors != "null":
         if sensor == "A":
@@ -58,10 +78,12 @@ class Visitors:
     Class representing a number of visitors.
     """
 
-    def generate_data(self) -> DataFrame:
+    def generate_data(self, df_or_dict: str) -> DataFrame | dict:
         """
         Generate a fictive Dataframe with the number of visitors per day and per hour for
         a store department.
+
+        :param df_or_dict: (str) the export type (dataframe or dictionary)
         :return: DataFrame
         """
         today = datetime.now()
@@ -85,8 +107,10 @@ class Visitors:
                     for sensor in sensors:
                         data = add_data(data, sensor, new_day, store, i_hour, 0.6, 0.7)
                         random.random()
-
-        return pd.DataFrame(data)
+        if df_or_dict == "df":
+            return pd.DataFrame(data)
+        elif df_or_dict == "dict":
+            return data
 
     def get_number_visitors(self, day: str, hour: int, store: str) -> int:
         """
@@ -98,7 +122,7 @@ class Visitors:
         :return: Dataframe
         """
         return (
-            self.generate_data()
+            self.generate_data("df")
             .query(
                 f"day == '{day}' and hour == {hour} and store_name == '{store}' and number_visitors != 'null'"
             )["number_visitors"]
@@ -112,6 +136,10 @@ class Visitors:
         :param day: (str) the day when we want to recover the number of visitors
         :return: dict
         """
-        data = self.generate_data().query(f"day == '{day}'").to_dict('list')
+        data = self.generate_data("dict")
+        index = [i for i, d in enumerate(data["day"]) if d == day]
+        filtered_data = {
+            key: [values[i] for i in index] for key, values in data.items()
+        }
 
-        return data
+        return filtered_data
